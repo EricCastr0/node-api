4bd5555fe32dcb3efeac20ec3b7c37a161526eda
Eric Castro <30558767+EricCastr0@users.noreply.github.com>
2022-12-15 14:25:41 -0300

    3commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'usse strict'
 
 const express = require('express');
+const bodyParse = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: false}));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -13,9 +17,24 @@ const route = router.get('/', (req, res, next) => {
 });
 
 const create = router.post('/', (req, res, next) => {
-    res.status(200).send();
-})
+    res.status(201).send(req.body);
+});
+
+const put = router.post('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
 
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
